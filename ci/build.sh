e27857656913526740f6d95cadc34706f3b0a694
Wes Schlichter <wschlichter@pivotal.io>
2019-05-23 10:57:35 -0500

    Updtate build script

--- ci/build.sh
+++ ci/build.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
 export TERM=${TERM:-dumb}
-cd tcs-embed-example
 
-./gradlew --no-daemon build
+SCRIPT="$0"
+
+# SCRIPT may be an arbitrarily deep series of symlinks. Loop until we have the concrete path.
+while [ -h "$SCRIPT" ] ; do
+  ls=`ls -ld "$SCRIPT"`
+  # Drop everything prior to ->
+  link=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$link" : '/.*' > /dev/null; then
+    SCRIPT="$link"
+  else
+    SCRIPT=`dirname "$SCRIPT"`/"$link"
+  fi  
+done
+
+# Change into our working directory
+cd `dirname ${SCRIPT}`/..
+
+./gradlew --no-daemon --info build
